db554238da84efb07fca64359948ac66ec63fd74
Dorus Leliveld <dorusleliveld@gmail.com>
2018-10-22 12:37:01 +0200

    Fixed some errors in kafka-start.bat

--- lab3/kafka-start.bat
+++ lab3/kafka-start.bat
@@ -1,8 +1,8 @@
-SET logdir="logs/"
+SET logdir="logs\"
 
 if not exist %logdir% mkdir %logdir%
 
-START %kafka_home%/bin/windows/zookeeper-server-start.bat %kafka_home%/config/zookeeper.properties > %logdir%
+START cmd /c %KAFKA_HOME%\bin\windows\zookeeper-server-start.bat %KAFKA_HOME%\config\zookeeper.properties ^> %logdir%zookeeper.log
 TIMEOUT 5 
-START %kafka_home%/bin/windows/kafka-server-start.bat %kafka_home%/config/server.properties > %logdir%
+START cmd /c %KAFKA_HOME%\bin\windows\kafka-server-start.bat %KAFKA_HOME%\config\server.properties ^> %logdir%kafka.log
 TIMEOUT 5
